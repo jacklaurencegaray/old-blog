a5e105ed2239cfaf8102da6c63fe97fd68b976c5
jacklaurencegaray <lawrencetecho@gmail.com>
2019-08-18 00:03:29 +0800

    Added some assets

---
+++ src/assets/styles/colors.ts
@@ -0,0 +1,21 @@
+const colors = {
+  dark: {
+    bg: "#1A1A1A",
+    secondaryBg: "#121212",
+    fg: "#FFFFFF",
+    primary: "#FFD49C",
+    secondary: "#FF9D1F",
+  },
+  light: {
+    bg: "#FFFFFF",
+    secondaryBg: "#F1F1F1",
+    fg: "#000000",
+    primary: "#FFB351",
+    secondary: "#FF9D1F",
+  },
+}
+
+const { light, dark } = colors
+
+export { light, dark }
+export default colors
---
+++ src/assets/styles/global.ts
@@ -0,0 +1,13 @@
+export default `
+    * {
+        transition: 500ms all;
+    }
+    html, body {
+        padding: 0;
+        margin: 0;
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+`
